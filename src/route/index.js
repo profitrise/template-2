1472febe3ccf432699e24477f334fbb0c4b77256
profitrise <marketing_olga@ukr.net>
2023-05-15 20:47:37 +0200

    my homework

--- src/route/index.js
+++ src/route/index.js
@@ -12,7 +12,9 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: 'default',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
